88ed72b179c3b91135405d76f7707af533c166aa
HARSH RAJ <harshraj250106@gmail.com>
2025-06-02 08:13:52 +0530

    Move btn--outline in -ve X direction

--- Js/app.js
+++ Js/app.js
@@ -4,3 +4,30 @@ const header = document.querySelector(".header");
 btnNav.addEventListener("click", function () {
     header.classList.toggle("nav-open");
 });
+
+// Smooth Scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+        // Scroll back to top
+        if (href === "#")
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+
+        // Scroll to other links
+        if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({ behavior: "smooth" });
+        }
+
+        // Close mobile navigation
+        if (link.classList.contains("main-nav-link"))
+            header.classList.toggle("nav-open");
+    });
+});
